0e2f361da1bd29e7b9594688ece378ae3ba15ddf
刘欢 <184555556@qq.com>
2020-08-14 20:58:12 +0800

    给图片设置dpi，解决图片没有dpi导致无法设置为壁纸问题

--- Bing.py
+++ Bing.py
@@ -5,6 +5,7 @@ import json
 import pymysql
 import os
 import logging
+from PIL import Image as ImagePIL, ImageFont, ImageDraw
 
 conn = pymysql.connect(host="127.0.0.1",
                        user="root",
@@ -16,7 +17,7 @@ FORMAT = "%(levelname)s %(lineno)d %(asctime)s %(threadName)s %(thread)d %(messa
 logging.basicConfig(format=FORMAT, level=logging.INFO, filename='log.log')
 
 logging.info('当前时间：%s 程序已经执行' %
-             (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),))
+             (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), ))
 
 
 def addslashes(s):
@@ -36,9 +37,9 @@ data = {
 
 headers = {
     'cookie':
-        "MUID=354A50E6331566ED28CF5EE5371565E7; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=652A02AB36A24C75907C72A928513C5F&dmnchg=1; MUIDB=354A50E6331566ED28CF5EE5371565E7; SRCHUSR=DOB=20191029&T=1572412007000; _EDGE_CD=u=zh-hans; _EDGE_S=mkt=zh-cn&ui=zh-hans&SID=19DC6D3A521F6F2F0B68633F53316E9B; SNRHOP=I=&TS=; ipv6=hit=1572428982179&t=4; SRCHHPGUSR=CW=1550&CH=729&DPR=1.2395833730697632&UTC=480&WTS=63708022181; _SS=SID=19DC6D3A521F6F2F0B68633F53316E9B&HV=1572425972&bIm=_FG",
+    "MUID=354A50E6331566ED28CF5EE5371565E7; SRCHD=AF=NOFORM; SRCHUID=V=2&GUID=652A02AB36A24C75907C72A928513C5F&dmnchg=1; MUIDB=354A50E6331566ED28CF5EE5371565E7; SRCHUSR=DOB=20191029&T=1572412007000; _EDGE_CD=u=zh-hans; _EDGE_S=mkt=zh-cn&ui=zh-hans&SID=19DC6D3A521F6F2F0B68633F53316E9B; SNRHOP=I=&TS=; ipv6=hit=1572428982179&t=4; SRCHHPGUSR=CW=1550&CH=729&DPR=1.2395833730697632&UTC=480&WTS=63708022181; _SS=SID=19DC6D3A521F6F2F0B68633F53316E9B&HV=1572425972&bIm=_FG",
     'user-agent':
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
 }
 
 response = requests.get(url, headers=headers, params=data)
@@ -48,16 +49,19 @@ res_json = res_json['images']
 
 r = requests.get(http + str(res_json[0]['url']), headers=headers)
 img = str(res_json[0]['enddate']) + '.jpg'
+path_url = '/data/flask_bing/App/static/images/'
 if r.status_code == 200:
-    with open('/data/flask_bing/App/static/images/' + img, 'wb') as f:
+    with open(path_url + img, 'wb') as f:
         f.write(r.content)
+    im = ImagePIL.open(path_url + img)
+    im.save(path_url + img, dpi=(200.0, 200.0))
 res_json = res_json[0]
 sql = " insert into bingImg(copyright,copyrightlink,startdate,fullstartdate, "
 sql += " enddate,hsh,url,imgUrl,http,bot,drk,title,top,wp,hs,addTime,imgName) "
 sql += " values ('%s','%s','%s','%s','%s','%s','%s','%s','%s',%d,%d,'%s',%d,'%s','%s',SYSDATE(),'%s') " % \
        (str(res_json['copyright']), str(res_json['copyrightlink']), str(res_json['startdate']),
         str(res_json['fullstartdate']), str(res_json['enddate']), str(res_json['hsh']), str(res_json['url']),
-        addslashes('/data/flask_bing/App/static/images/' + img), http, int(res_json['bot']), int(res_json['drk']),
+        addslashes(path_url + img), http, int(res_json['bot']), int(res_json['drk']),
         str(res_json['title']), int(res_json['top']), str(res_json['wp']), str(res_json['hs']), img)
 cursor.execute(sql)
 conn.commit()
